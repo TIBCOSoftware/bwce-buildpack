784725d4b383b1dc14b4e76ba22d96176ef8b618
justin8888 <jusitnmd@126.com>
2016-01-05 18:22:35 -0800

    more generic

--- test/testBuildpack.sh
+++ test/testBuildpack.sh
@@ -9,10 +9,11 @@ fi
 echo "***** Starting BWCE Sanity *****"
 
 echo "***** Pushing HTTP Greetings App to CF *****"
+appName=`grep -E "name:" manifest.yml | cut -d ':' -f 2 | sed 's/^ *//g' | sed 's/ *$//g'`
 URL=`cf push -f manifest.yml | grep "urls:" | cut -d ' ' -f 2`
 sleep 5
 a=$(curl "http://$URL/greetings/")
-BWCE_MESSAGE=`grep -E "RESPONSE_MESSAGE:" manifest.yml | cut -d ':' -f 2 | tr -d ' ' `
+BWCE_MESSAGE=`grep -E "RESPONSE_MESSAGE:" manifest.yml | cut -d ':' -f 2 | sed 's/^ *//g' | sed 's/ *$//g' `
 if [ "${a}" = "Greetings from $BWCE_MESSAGE" ]; then
     echo "----------------------------------------------------------------------";
         echo "******* HTTP Greetings App - running successfully! *******";
@@ -26,9 +27,9 @@ else
     echo "----------------------------------------------------------------------";
     echo "******* bwce-buildpack test Failed ! *******";
     echo "----------------------------------------------------------------------";
-    cf delete httpapp -f
+    cf delete $appName -f
     exit -1
 
 fi
 
-cf delete httpapp -f
+cf delete $appName -f
