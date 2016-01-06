6319acb78358199a01ddffd4e8b125136bc43eea
justin8888 <jusitnmd@126.com>
2016-01-05 17:45:34 -0800

    add buildpack checks

--- test/testBuildpack.sh
+++ test/testBuildpack.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+OUTPUT=`cf buildpacks | grep "bwce-buildpack"`
+if [ -z "$OUTPUT" ]; then
+    echo "bwce-buildpack is not in the buildpacks. Please run the uploadBuildpack.sh first."
+    exit 1
+fi
+
+
 echo "***** Starting BW Container Sanity *****"
 
 echo "***** Pushing HTTP Greetings App to CF *****"
@@ -17,7 +24,7 @@ else
     echo "******* HTTP Greetings App Failed ! Deleting App *******";
     echo "----------------------------------------------------------------------";
     echo "----------------------------------------------------------------------";
-    echo "******* bw-buildpack test Failed ! *******";
+    echo "******* bwce-buildpack test Failed ! *******";
     echo "----------------------------------------------------------------------";
     cf delete httpapp -f
     exit -1
