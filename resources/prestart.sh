d91c7020337729c0a987cd7dbad5281809bd5caa
wgu <wgu@tibco.com>
2016-10-18 15:18:39 -0700

    https://jira.tibco.com/browse/BWCE-763

--- resources/prestart.sh
+++ resources/prestart.sh
@@ -2,7 +2,6 @@
 # Copyright (c) 2016, TIBCO Software Inc. All rights reserved.
 # You may not use this file except in compliance with the license 
 # terms contained in the TIBCO License.md file provided with this file.
-
 printBWTable ()
 {
 	echo "---------------> Product Inventory"
@@ -30,12 +29,25 @@ do
 done
 fi
 }
+checkJAVAHOME()
+{
+		if [[ ${JAVA_HOME}  ]]; then
+ 			echo $JAVA_HOME
+ 		else
+			JRE_VERSION=`ls $APPDIR/tibco.home/tibcojre64/`
+			jreLink=tibcojre64/$JRE_VERSION
+			chmod +x $APPDIR/tibco.home/$jreLink/bin/java
+			chmod +x $APPDIR/tibco.home/$jreLink/bin/javac
+			export JAVA_HOME=$APPDIR/tibco.home/$jreLink
+ 		fi
+}
 export APPDIR=/home/vcap/app
 export BW_KEYSTORE_PATH=$HOME/keystore
 export MALLOC_ARENA_MAX=2
 export MALLOC_MMAP_THRESHOLD_=1024
 export MALLOC_TRIM_THRESHOLD_=1024
 export MALLOC_MMAP_MAX_=65536
+export TIB_DTCP_EXTERNAL={$CF_INSTANCE_IP}{$PORT/$CF_INSTANCE_PORT}
 chmod 755 $APPDIR/tibco.home/bw*/*/bin/startBWAppNode.sh
 sed -i.bak "s#_APPDIR_#$APPDIR#g" $APPDIR/tibco.home/bw*/*/config/appnode_config.ini
 if [ "$(ls $APPDIR/tibco.home/bw*/*/ext/shared)"  ]; then 
@@ -64,12 +76,9 @@ if grep -q BW.CLOUD.PORT "$APPDIR/tmp/pcf.substvar"; then
 fi
 
 export JETTISON_JAR=`echo $APPDIR/tibco.home/bw*/*/system/shared/com.tibco.bw.tpcl.org.codehaus.jettison*/jettison*.jar`
-JRE_VERSION=`ls $APPDIR/tibco.home/tibcojre64/`
-jreLink=tibcojre64/$JRE_VERSION
-chmod +x $APPDIR/tibco.home/$jreLink/bin/java
-chmod +x $APPDIR/tibco.home/$jreLink/bin/javac
-$APPDIR/tibco.home/$jreLink/bin/javac -cp $JETTISON_JAR:.:$APPDIR/tibco.home/$jreLink/lib:$APPDIR/tibco.home/$jreLink/bin ProfileTokenResolver.java
-$APPDIR/tibco.home/$jreLink/bin/java -cp $JETTISON_JAR:.:$APPDIR/tibco.home/$jreLink/lib:$APPDIR/tibco.home/$jreLink/bin ProfileTokenResolver
+checkJAVAHOME
+$JAVA_HOME/bin/javac -cp $JETTISON_JAR:.:$JAVA_HOME/lib ProfileTokenResolver.java
+$JAVA_HOME/bin/java -cp $JETTISON_JAR:.:$JAVA_HOME/lib ProfileTokenResolver
 
 STATUS=$?
 if [ $STATUS == "1" ]; then
